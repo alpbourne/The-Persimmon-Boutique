4bcd47ffc40e2a1ba6559b4ade3f4cb66ab6d733
alpbourne <alpbourne@gmail.com>
2018-04-09 20:49:59 -0400

    finished create function in Items Controller, added update, destroy, set_item, item_params, methods, and before set_item before action

--- the-persimmon-boutique-api/app/controllers/api/items_controller.rb
+++ the-persimmon-boutique-api/app/controllers/api/items_controller.rb
@@ -1,4 +1,5 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_surfboard, only: [:show, :edit, :destroy]
 
   def index
     render json: Item.all
@@ -6,15 +7,40 @@ class Api::ItemsController < ApplicationController
 
   def create
     item = Item.new[item_params]
+    if item.save
+      render json: item
+    else
+      render json: {message: "Item could not be saved"}, status: 400
+    end
   end
 
   def show
-    render json: Item.find_by[:id params[:id]]
+    render json: @item
   end
+
+  # def update
+  #   if @item.update(item_params)
+  #     render json: @item
+  #   else
+  #     render json: {message: "Item could not be saved."}, status: 400
+  #   end
+  # end
+  #
+  # def destroy
+  #   if @item.destroy
+  #     render json: {message: "Successfully Destroyed!"}, status: 204
+  #   else
+  #     render json: {message: "Item could not be removed."}, status: 400
+  #   end
+  # end
+
 
   private
     def item_params
       params.require[:item].permit[:name, :price, :image]
     end
 
+    def set_item
+      @item = Item.find_by(id: params[:id])
+    end
 end
