e1cc2f068ce85fd009387f98bdef4c2e9fd63319
alpbourne <alpbourne@gmail.com>
2018-04-10 12:54:02 -0400

    added constructor and componentDidMount to App.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -22,10 +22,25 @@ const items = [
 ]
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/items')
+      .then(response => response.json())
+      .then(items => this.setState({ items }))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <Items items={items}/>
+        <Items items={this.state.items}/>
       </div>
     );
   }
