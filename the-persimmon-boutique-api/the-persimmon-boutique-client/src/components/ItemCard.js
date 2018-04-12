2ca6b204a19c085d787c2afd47cdd703d124d4bb
alpbourne <alpbourne@gmail.com>
2018-04-12 11:23:26 -0400

    added ItemCard.js component and components folder, moved code from Item.js to ItemCard.js

---
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/ItemCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ItemCard = ({ item }) => (
+  <div key={item.id} className="ItemCard">
+    <img className="ItemImage" src={item.image} alt={item.name} />
+    <h3>{item.name}</h3>
+    <p>Price: ${item.price}</p>
+  </div>
+)
+
+export default ItemCard;
--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/Items.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/Items.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import ItemCard from '../components/ItemCard';
 import './Items.css';
 
 const Items = (props) => (
   <div className="ItemsContainer">
     <h1>Our Collection</h1>
-    {props.items.map(item =>
-      <div key={item.id} className="ItemCard">
-        <img className="ItemImage" src={item.image} alt={item.name} />
-        <h3>{item.name}</h3>
-        <p>Price: ${item.price}</p>
-      </div>
-    )}
+    {props.items.map(item => <ItemCard item={item} />)}
   </div>
 );
 
