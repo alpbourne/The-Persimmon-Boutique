5712801f09975b35df949823c785b7130dc1ff54
alpbourne <alpbourne@gmail.com>
2018-04-12 16:07:00 -0400

    added data and changed code in store.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/store.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/store.js
@@ -8,12 +8,22 @@ import thunk from 'redux-thunk';
 const reviewsReducer = (state = [], action) => {
   switch(action.type) {
     case 'ADD_REVIEW':
-      return action.surfboard;
+      return action.item;
     case 'DELETE_REVIEW':
-      return action.surfboard
+      return action.item;
     default:
       return state;
   }
 }
 
-const reviews = reviewsReducer(null, {type: '@@INIT' });
+const reducers = combineReducers({
+  reviews: reviewsReducer
+});
+
+const middleware = [thunk];
+
+export default createStore(
+  reducers,
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
