f2942f200691c1b4f4651e99c889f1b2ea20f7c8
alpbourne <alpbourne@gmail.com>
2018-04-12 16:56:50 -0400

    added render in ReviewInput.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/ReviewInput.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/ReviewInput.js
@@ -27,3 +27,17 @@ class ReviewInput extends Component {
       text: '',
     });
   }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input type="text" value={this.state.text} onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+};
+
+export default ReviewInput;
