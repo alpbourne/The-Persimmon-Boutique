8c90e8284140ca66198f1831c89d42182c3024b8
alpbourne <alpbourne@gmail.com>
2018-04-12 14:25:38 -0400

    added create and review_params function to reviews controller

--- the-persimmon-boutique-api/app/controllers/api/reviews_controller.rb
+++ the-persimmon-boutique-api/app/controllers/api/reviews_controller.rb
@@ -4,3 +4,18 @@ class Api::ReviewsController < ApplicationController
     render json: Review.all
   end
 end
+
+  def create
+    review = Review.new[review_params]
+    if review.save
+      render json: review
+    else
+      render json: {message: "Review could not be saved"}, status: 400
+    end
+  end
+
+  private
+
+  def review_params
+    params.require[:review].permit[:author, :body]
+  end
