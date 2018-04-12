12762ce64106a6312656f5693b06f639cb29632e
alpbourne <alpbourne@gmail.com>
2018-04-12 12:43:36 -0400

    added Please tell us how we are doing! line at bottom to start creating reviews component

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <Items items={this.state.items}/>
+        <h3>Please tell us how we are doing! </h3>
       </div>
     );
   }
