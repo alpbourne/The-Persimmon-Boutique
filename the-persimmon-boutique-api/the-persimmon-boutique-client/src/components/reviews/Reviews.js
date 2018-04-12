1d00dcd04eb805cc2571f7dc20a13af405492286
alpbourne <alpbourne@gmail.com>
2018-04-12 16:59:01 -0400

    added Reviews.js file

---
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/Reviews.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Review from './Review';
+
+class Reviews extends Component {
+  render() {
+    const reviews = this.props.store.getState().reviews.map((review, index) => {
+      return <Review review={review} key={index} store={this.props.store} />
+    });
+
+    return(
+      <ul>
+        {reviews}
+      </ul>
+    );
+  }
+};
+
+export default Reviews;
