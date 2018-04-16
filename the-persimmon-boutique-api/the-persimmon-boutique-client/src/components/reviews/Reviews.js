ac83fe18ca6642327e824612de2071bee8631ebc
alpbourne <alpbourne@gmail.com>
2018-04-16 12:23:53 -0400

    edited Review.js code

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/Reviews.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/Reviews.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ReviewInput from './ReviewInput';
 import Review from './Review';
 
 class Reviews extends Component {
-  render() {
-    const reviews = this.props.store.getState().reviews.map((review, index) => {
-      return <Review review={review} key={index} store={this.props.store} />
-    });
-
+  this.
+  render()
     return(
-      <ul>
-        {reviews}
-      </ul>
+      <div className="ReviewsContainer">
+        <h2>Reviews</h2>
+        <ul>
+          {this.props.reviews.map(review => <ReviewCard key={review.id} review={review} />)}
+        </ul>
+        <ReviewInput />
     );
   }
 };
 
-export default Reviews;
+export default connect()(Reviews);
