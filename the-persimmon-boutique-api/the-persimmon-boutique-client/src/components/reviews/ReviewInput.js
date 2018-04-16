e5bc771eae1f36984be4019169c0967c14b0ee8a
alpbourne <alpbourne@gmail.com>
2018-04-16 16:13:45 -0400

    commented out older ReviewInput.js code and edited/added new code also

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/ReviewInput.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/ReviewInput.js
@@ -2,46 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class ReviewInput extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      author: '',
-      body: '',
-    };
-  }
-
-  handleOnChange(event) {
-    this.setState({
-      author: event.target.value,
-      body: event.target.value,
-    });
-  }
-
-  handleOnSubmit(event) {
-    event.preventDefault();
-    this.props.store.dispatch({
-      type: 'ADD_REVIEW',
-      review: {
-        author: this.state.author,
-        body: this.state.body,
-      },
-    });
-    this.setState({
-      author: '',
-      body: '',
-    });
-  }
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this.state = {
+  //     author: '',
+  //     body: '',
+  //   };
+  // }
+  //
+  // handleOnChange(event) {
+  //   this.setState({
+  //     author: event.target.value,
+  //     body: event.target.value,
+  //   });
+  // }
+  //
+  // handleOnSubmit(event) {
+  //   event.preventDefault();
+  //   this.props.store.dispatch({
+  //     type: 'ADD_REVIEW',
+  //     review: {
+  //       author: this.state.author,
+  //       body: this.state.body,
+  //     },
+  //   });
+  //   this.setState({
+  //     author: '',
+  //     body: '',
+  //   });
+  // }
 
   render() {
+    const {author, body} = this.props.reviewFormData;
     return (
       <div>
         <h3>Please tell us how we are doing! </h3>
         <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <label htmlFor="author">Author:</label>
-          <input type="text" value={this.state.author} onChange={(event) => this.handleOnChange(event)} />
-          <label htmlFor="body">Review:</label>
-          <input type="text" value={this.state.body} onChange={(event) => this.handleOnChange(event)} />
+          <div>
+            <label htmlFor="author">Author:</label>
+            <input type="text" name="author" value={author} onChange={(event) => this.handleOnChange(event)} />
+          </div>
+          <div>
+            <label htmlFor="body">Review:</label>
+            <input type="text" name="body" value={body} onChange={(event) => this.handleOnChange(event)} />
+          </div>
           <input type="submit" />
         </form>
       </div>
