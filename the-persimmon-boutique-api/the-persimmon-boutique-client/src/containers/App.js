accecfba50d1afc95d3fa329d93eaabe095c341b
alpbourne <alpbourne@gmail.com>
2018-04-18 15:26:41 -0400

    set up router in App.js, home page and add reviews functionality still works

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -1,3 +1,15 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const App = (props) => {
+  return (
+    <Router>
+      <div className="app">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
+  );
+};
+
+export default App
