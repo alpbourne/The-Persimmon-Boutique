b7b1692e53a3a0ed7d6ce7fb115b2d5803db3df0
alpbourne <alpbourne@gmail.com>
2018-04-18 15:39:07 -0400

    added About route and css

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import About from '../components/About';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = (props) => {
@@ -7,6 +8,7 @@ const App = (props) => {
     <Router>
       <div className="app">
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
       </div>
     </Router>
   );
