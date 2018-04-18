d4a9913b5d21accb9ae7d030bb67ac279b99b32b
alpbourne <alpbourne@gmail.com>
2018-04-18 15:24:24 -0400

    deleted code in App.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -1,32 +1,3 @@
 import React, { Component } from 'react';
 import Home from './Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-        items: [],
-        reviews: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/api/items')
-      .then(response => response.json())
-      .then(items => this.setState({ items }))
-  }
-
-  render() {
-    console.log(this.state)
-    return (
-      <div className="App">
-        <Items items={this.state.items}/>
-        <Reviews store={this.props.store}/>
-      </div>
-    );
-  }
-}
-
-export default App;
