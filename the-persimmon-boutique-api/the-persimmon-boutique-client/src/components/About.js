10bd3b1e5080bf0e5fc9e3d5bf4d78ece35bfca3
alpbourne <alpbourne@gmail.com>
2018-04-18 15:35:34 -0400

    added About file

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-
+    <div className="about">
+      <h2>About The Persimmon Boutique</h2>
+      <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
     </div>
   );
 };
