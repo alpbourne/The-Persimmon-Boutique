3f58b6cb0729ebbb14ade549a82f5c7573e384d0
alpbourne <alpbourne@gmail.com>
2018-04-19 13:16:09 -0400

    changed css on About.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
@@ -4,8 +4,8 @@ import './Routes.css';
 const About = () => {
   return (
     <div className="AboutContainer">
+      <h2>About Us</h2>
       <div className="AboutCard">
-        <h2>About The Persimmon Boutique</h2>
         <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
       </div>
     </div>
