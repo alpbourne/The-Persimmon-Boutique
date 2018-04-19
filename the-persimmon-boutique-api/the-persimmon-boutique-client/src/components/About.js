59a373fa1814e2cc989ef4349ce3a2952fd352c3
alpbourne <alpbourne@gmail.com>
2018-04-19 12:45:45 -0400

    added Routes.css file and added css to About component

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Routes.css';
 
 const About = () => {
   return (
-    <div className="about">
-      <h2>About The Persimmon Boutique</h2>
-      <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
+    <div className="AboutContainer">
+      <div className="AboutCard">
+        <h2>About The Persimmon Boutique</h2>
+        <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
+      </div>
     </div>
   );
 };
