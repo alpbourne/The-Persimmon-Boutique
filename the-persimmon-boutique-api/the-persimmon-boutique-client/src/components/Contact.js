e712c689d9f35f1d452dcfe49dae9519ccde0e40
alpbourne <alpbourne@gmail.com>
2018-04-20 12:52:18 -0400

    added Contact page

---
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Routes.css';
+
+const About = () => {
+  return (
+    <div className="ContactContainer">
+      <h2>Contact Us</h2>
+      <div className="ContactCard">
+        <h3>Our address:</h3>
+        <p>1 Main Street</p>
+        <p>Gerber, MD 23456</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
