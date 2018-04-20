8fe62da56fb1822a97fe67ec866fc225bd34e3d6
alpbourne <alpbourne@gmail.com>
2018-04-20 12:58:07 -0400

    added Contact page css

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
@@ -6,9 +6,11 @@ const About = () => {
     <div className="ContactContainer">
       <h2>Contact Us</h2>
       <div className="ContactCard">
-        <h3>Our address:</h3>
-        <p>1 Main Street</p>
-        <p>Gerber, MD 23456</p>
+        <p><b>Address: </b>1 Main Street, Gerber, MD 23456</p>
+        <p><b>Email Address: <b/>ThePersimmonBoutique@gmail.com</p>
+        <p><b>Facebook Page: <b/>The Persimmon Boutique</p>
+        <p><b>Twitter: </b>@ThePersimmonBoutique</p>
+        <p><b>Instagram: </b>@ThePersimmonBoutique</p>
       </div>
     </div>
   );
