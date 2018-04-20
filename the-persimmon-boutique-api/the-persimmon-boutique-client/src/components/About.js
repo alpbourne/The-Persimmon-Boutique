6a195d41dda95372fb695f12c3f7425fd2c856e3
alpbourne <alpbourne@gmail.com>
2018-04-20 15:39:31 -0400

    added css and image to About page

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Routes.css';
+import aboutPage from './aboutPage.jpg'
 
 const About = () => {
   return (
     <div className="AboutContainer">
       <h2>About Us</h2>
       <div className="AboutCard">
+        <img width={598} height={300} alt="We Are Open" src={aboutPage} />
         <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
       </div>
     </div>
