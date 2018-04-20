6a1484dfb2faa44db4ba69bc6dfdce706bb85ce0
alpbourne <alpbourne@gmail.com>
2018-04-20 13:47:19 -0400

    added contact to Router and fixed closures on Contact.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
@@ -7,8 +7,8 @@ const About = () => {
       <h2>Contact Us</h2>
       <div className="ContactCard">
         <p><b>Address: </b>1 Main Street, Gerber, MD 23456</p>
-        <p><b>Email Address: <b/>ThePersimmonBoutique@gmail.com</p>
-        <p><b>Facebook Page: <b/>The Persimmon Boutique</p>
+        <p><b>Email Address: </b>ThePersimmonBoutique@gmail.com</p>
+        <p><b>Facebook Page: </b>The Persimmon Boutique</p>
         <p><b>Twitter: </b>@ThePersimmonBoutique</p>
         <p><b>Instagram: </b>@ThePersimmonBoutique</p>
       </div>
--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from '../components/Home';
 import About from '../components/About';
+import Contact from '../components/Contact';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import './App.css';
@@ -13,6 +14,7 @@ const App = (props) => {
         <h1> The Persimmon Boutique </h1>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </div>
     </Router>
   );
