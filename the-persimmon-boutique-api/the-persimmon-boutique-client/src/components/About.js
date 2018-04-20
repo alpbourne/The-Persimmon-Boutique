3a5b540f3a8ebcfa01aa9084f2359ebf5d74c1c8
alpbourne <alpbourne@gmail.com>
2018-04-20 15:56:06 -0400

    added writing and css to about page

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/About.js
@@ -8,7 +8,8 @@ const About = () => {
       <h2>About Us</h2>
       <div className="AboutCard">
         <img width={598} height={300} alt="We Are Open" src={aboutPage} />
-        <p>The Perisommon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street.</p>
+        <p>The Persimmon Boutique first opened up on July 1, 1986. We are a family owned lifestyle store located on Main Street. Originally we sold mostly refurbished furniture, but as time went one we gradually started adding all types lifestyle items. Today we sell kitchenware, clothes, jewelry, home decor, and other adorable miscellaneous items.</p>
+        <p>If you have any suggesstions or encouragement for us, we would love for you to write a review on our homepage. We love customer feedback! Please stop by and introduce yourself to us next time you come in. We enjoy meeting and chatting with our customers!</p>
       </div>
     </div>
   );
