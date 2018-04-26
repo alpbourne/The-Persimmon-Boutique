b85b00dff4984f89f2cb92e3ac4add31f83c11ba
alpbourne <alpbourne@gmail.com>
2018-04-25 22:02:42 -0400

    deleted unused code in reviews/Review.js

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/Review.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/reviews/Review.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
-  // handleOnClick = (review) => {
-  //   this.props.deleteReview(review.id);
-  // }
-
   render() {
     const { review, deleteReview } = this.props
     return (
@@ -20,16 +16,3 @@ class Review extends Component {
 };
 
 export default Review;
-
-// import React from 'react';
-//
-// const ReviewCard = ({ review }) => (
-//   <div className="ReviewCard">
-//     <li>
-//       {this.props.review.author}
-//       {this.props.review.body}
-//     </li>
-//   </div>
-// );
-//
-// export default ReviewCard;
