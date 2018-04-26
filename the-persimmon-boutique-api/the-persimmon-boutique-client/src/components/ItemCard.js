a8fdd47d661bd5bf464126f56a2f9d27099bd987
alpbourne <alpbourne@gmail.com>
2018-04-26 18:14:38 -0400

    like_count is being rendered from API

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/ItemCard.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/ItemCard.js
@@ -3,23 +3,23 @@ import { connect } from 'react-redux';
 import '../containers/Items.css';
 
 class ItemCard extends Component {
-  state = {
-    counter: 0
-  };
+  // state = {
+  //   counter: 0
+  // };
 
   handleOnClick = event => {
     this.setState({counter: this.state.counter + 1})
   }
 
   render() {
-    const {image, name, price, counter} = this.props.item;
+    const {image, name, price, like_count} = this.props.item;
     return (
       <div className="ItemCard">
         <img className="ItemImage" src={image} alt={name} width={275} height={200}/>
         <h3>{name}</h3>
         <p>Price: ${price}</p>
         <button onClick={this.handleOnClick}>Like</button>
-        Counter: {counter}
+        Counter: {like_count}
       </div>
     )
   }
@@ -31,9 +31,7 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {
-  increaseCount
-})(ItemCard);
+export default connect(mapStateToProps)(ItemCard);
 
 //
 // } ({ item }) => (
