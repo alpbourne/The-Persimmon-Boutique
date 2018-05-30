c306581d9d6d7f49cbdd8d8dd6bbf65c58ee5d2b
alpbourne <alpbourne@gmail.com>
2018-05-30 15:38:05 -0400

    changed css on Contact and About page to be responsive to different browser sizes

--- the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
+++ the-persimmon-boutique-api/the-persimmon-boutique-client/src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
 
       <h2>Contact Us</h2>
       <div className="ContactCard">
-        <img width={598} height={300} alt="We Are Open" src={contactPage} />
+        <img className="ContactImage" width={598} height={300} alt="We Are Open" src={contactPage} />
         <p><b>Address: </b>1 Main Street, Gerber, MD 23456</p>
         <p><b>Email Address: </b>ThePersimmonBoutique@gmail.com</p>
         <p><b>Facebook Page: </b>The Persimmon Boutique</p>
